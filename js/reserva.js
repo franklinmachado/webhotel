6088c8a081f6804d866da3e72c331ceeebaeee3e
lucasalmeida <lac.lucas96@gmail.com>
2023-01-19 22:40:44 -0300

    adiciona dados de reserva em localStorage

--- js/reserva.js
+++ js/reserva.js
@@ -1,4 +1,12 @@
-const chales = document.querySelectorAll("input[name=chale]");
+const chales = document.querySelectorAll("input[name='chale']")
+const checkIn = document.querySelector("#check_in")
+const checkOut = document.querySelector("#check_out")
+const quantidadeDePessoas = document.querySelector("#quantidade_pessoas")
+
+const modalQuarto = document.querySelector("#quarto")
+const modalCheckIn = document.querySelector("#checkIn")
+const modalCheckOut = document.querySelector("#checkOut")
+const modalQtdPessoas = document.querySelector("#qtdPessoas")
 
 const reserva = {
     checkIn: null,
@@ -6,16 +14,37 @@ const reserva = {
     quantidadeDePessoas: 0,
     quarto: {
         tipo: null,
-        valor: 0
+        valor: 0,
     },
     total: 0,
 }
 
+checkIn.addEventListener("input", () => {
+    reserva.checkIn = checkIn.value
+    updateReserva(reserva)
+})
+
+checkOut.addEventListener("input", () => {
+    reserva.checkOut = checkOut.value
+    updateReserva(reserva)
+})
+
+quantidadeDePessoas.addEventListener("input", () => {
+    reserva.quantidadeDePessoas = quantidadeDePessoas.value
+    updateReserva(reserva)
+})
+
 chales.forEach((chale) => {
-    chale.addEventListener("click", () => {
-        if (chale.checked) {
-            reserva.quarto.tipo = chale.value;
-            console.log(reserva);
-        }
+    chale.addEventListener("input", () => {
+        reserva.quarto.tipo = chale.value
+        updateReserva(reserva)
     })
 })
+
+function updateReserva(reserva) {
+    localStorage.setItem("reserva", JSON.stringify(reserva))
+    modalQuarto.textContent = reserva.quarto.tipo
+    modalCheckIn.textContent = reserva.checkIn
+    modalCheckOut.textContent = reserva.checkOut
+    modalQtdPessoas.textContent = reserva.quantidadeDePessoas
+}
